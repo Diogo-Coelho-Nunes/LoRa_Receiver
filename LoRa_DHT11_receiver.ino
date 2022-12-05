f71e6dfcac96746d39e87312ce11f22fbcf87ff3
Diogo Coelho Nunes <105322991+Diogo-Coelho-Nunes@users.noreply.github.com>
2022-12-05 13:42:17 +0000

    Initial commit

---
+++ LoRa_DHT11_receiver.ino
@@ -0,0 +1,187 @@
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Import WiFI library
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+#include <AsyncTCP.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Libraries to get time from NTP Server
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//LoRa frequency for Europe
+#define BAND 866E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+const char* ssid     = "Nunes_hotspot";
+const char* password = "nunescoelho";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+String LoRaData;
+
+String formattedDate;
+String day;
+String hour;
+String timestamp;
+String temperature;
+String humidity;
+String pressure;
+String readingID;
+
+
+//Server connection on port 80
+AsyncWebServer server(80);
+
+
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return temperature;
+  }
+  else if(var == "HUMIDITY"){
+    return humidity;
+  }
+  else if(var == "PRESSURE"){
+    return pressure;
+  }
+  else if(var == "TIMESTAMP"){
+    return timestamp;
+  }
+  return String();
+}
+
+//Wifi Connection function
+void connectWiFi(){
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  display.setCursor(0,20);
+  display.print("Access web server at: ");
+  display.setCursor(0,30);
+  display.print(WiFi.localIP());
+  display.display();
+}
+
+void setup() { 
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+   if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+   }
+ 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "data/index.html", String(), false, processor);
+  });
+ 
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA RECEIVER ");
+  display.display();
+  
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.println("LoRa Initializing OK!");
+  display.display();
+
+  server.begin();
+  connectWiFi();
+}
+
+void loop() {
+  String Temp = LoRaData.substring(0,5);
+  String Hum = LoRaData.substring(5,10);
+  String Soil = LoRaData.substring(10,15);
+  
+//try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    //received a packet
+    Serial.print("Received packet ");
+
+    //read packet
+    while (LoRa.available()) {
+      LoRaData = LoRa.readString();
+      Serial.print(LoRaData);
+    }
+
+    //print RSSI of packet
+    int rssi = LoRa.packetRssi();
+    Serial.print(" with RSSI ");    
+    Serial.println(rssi);
+
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print("LORA RECEIVER");
+    display.setCursor(0,20);
+    display.print("Temperature:");
+    display.print(Temp);
+    display.setCursor(0,30);
+    display.print("AirHumidity:");
+    display.print(Hum);
+    display.setCursor(0,40);
+    display.print("SoilMoisture:");
+    display.print(Soil);
+    
+    display.display();
+    }
+}
